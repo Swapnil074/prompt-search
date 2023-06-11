a1dbfc32c44c33793c304759b3a6687e8ac5bf3b
Swapnil074 <mailswapnil0074@gmail.com>
2023-06-11 20:10:59 +0530

    feat: google authentication and database connect

--- components/Nav.jsx
+++ components/Nav.jsx
@@ -5,15 +5,15 @@ import { useState, useEffect } from 'react'
 import {signIn,signOut,useSession,getProviders} from 'next-auth/react'
 
 const Nav = () => {
-  const isUserLoggedIn=true;
+  const {data:session}=useSession();
   const [providers, setProviders]= useState(null)
   const [toggleDropDown, setToggleDropDown]=useState(false)
   useEffect(()=>{
     const setUserContext=async()=>{
       const res=await getProviders()
-      setUserContext(res)
+      setProviders(res)
     }
-    setProviders();
+    setUserContext();
   },[])
   return (
     <nav className="flex-between w-full mb-16 pt-3">
@@ -27,11 +27,10 @@ const Nav = () => {
         />
         <p className="logo_text">Promptopia</p>
       </Link>
-
       {/*Web Navigation*/}
 
       <div className="sm:flex hidden">
-        {isUserLoggedIn ? (
+        {session?.user ? (
           <div className="flex gap-3 md:gap-5">
             <Link href="/create-prompt" className="black_btn ">
               Create Post
@@ -41,7 +40,7 @@ const Nav = () => {
             </button>
             <Link href="/profile">
               <Image
-                src="/assets/images/logo.svg"
+                src={session.user.image}
                 width={37}
                 height={37}
                 className="rounded-full"
@@ -69,10 +68,10 @@ const Nav = () => {
       {/*Mobile Navigation*/}
 
       <div className="sm:hidden flex relative">
-        {isUserLoggedIn ? (
+        {session?.user ? (
           <div className="flex">
             <Image
-              src="/assets/images/logo.svg"
+              src={session.user.image}
               width={37}
               height={37}
               className="rounded-full"
