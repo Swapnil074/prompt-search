3587900147426b872c32cd9943b75d1450cd4b76
Swapnil074 <mailswapnil0074@gmail.com>
2023-06-28 15:40:11 +0530

    feat: nav redirect

--- components/Nav.jsx
+++ components/Nav.jsx
@@ -25,7 +25,7 @@ const Nav = () => {
           height={30}
           className="object-contain"
         />
-        <p className="logo_text">Promptopia</p>
+        <p className="logo_text"></p>
       </Link>
       {/*Web Navigation*/}
 
